ffb5128d034e9a1106602deae3d0176fbe41a97b
Georgegitahi <petergeorgembugua724@gmail.com>
2024-03-27 00:50:33 +0300

    scope lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+function shop(){
+    customerName='bob';
+    return "shop () function invoked!"
+}
+shop();
+function upperCaseCustomerName(){
+customerName= 'bob'.toUpperCase()
+}
+shop()
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+shop()
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+
+}
+shop()
+const leastFavoriteCustomer = "";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'kimberly'
+    
+  }
